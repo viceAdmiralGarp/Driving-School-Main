5d2a3cdcc8f4392c719232017852029417303d80
viceAdmiralGarp <vyacheslavtihonov636@gmail.com>
2024-06-12 22:59:31 +0300

    sanya

--- mmdev/js/registration.js
+++ mmdev/js/registration.js
@@ -2,24 +2,28 @@ $(document).ready(function(){
     $('#registration-form').submit(function(event){
         event.preventDefault();
 
+        var fullname = $('.registration-input[name="fullname"]').val();
+        var email = $('.registration-input[name="email"]').val();
+        var phone = $('.registration-input[name="phone"]').val();
+
         var formData = {
-            fullname: $('#fullname').val(),
-            email: $('#email').val(),
-            phone: $('#phone').val()
+            fullname: fullname,
+            email: email,
+            phone: phone
         };
 
         $.ajax({
             type: 'POST',
             url: 'http://localhost:8080/register',
             data: formData,
-            contentType: 'application/json',
+            dataType: 'json',
             success: function(response){
                 console.log(response);
-                alert('Registration successful!');
+                alert('Регистрация успешна!');
             },
             error: function(xhr, status, error){
                 console.error(xhr.responseText);
-                alert('Error registering: ' + xhr.responseText);
+                alert('Ошибка регистрации: ' + xhr.responseText);
             }
         });
     });
