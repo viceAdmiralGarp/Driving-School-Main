f6440f9f726e4a155f8fd66ed1fba8754cdfdbd9
viceAdmiralGarp <vyacheslavtihonov636@gmail.com>
2024-06-16 19:57:15 +0300

    trns

--- mmdev/js/languages.js
+++ mmdev/js/languages.js
@@ -8,16 +8,58 @@ const translations = {
         contacts: "Contacts",
         comments: "Comments",
         language: "Language",
-        hero_text: "One click and\n" +
-            "the car license\n" +
-            "are in your pocket",
+        hero_text: "One click and<br>the car license<br>are in your pocket",
         button_sing_up: "Sign up for a course",
-        q_i:"Qualified instructors",
-        h_f_e:"Help in preparing for exams",
-        m_e_m:"Modern educational materials",
-        m_v_f:"Modernized vehicle fleet",
-        c_p:"Competitive prices",
-        about_us_main_text:"ABOUT US"
+        q_i: "Qualified instructors",
+        h_f_e: "Help in preparing for exams",
+        m_e_m: "Modern educational materials",
+        m_v_f: "Modernized vehicle fleet",
+        c_p: "Competitive prices",
+        about_us_main_text: "About <span class='blue'>Us</span>",
+        about_us_sub_text1: "<br><br>We are glad to welcome you to the website of the driving school in Dnipro <span class=\"blue\">“Grand”</span>.\n" +
+            "                    <br><br>With us you will learn how to <span class=\"blue\">efficiently drive</span> a car with a\n" +
+            "                    manual or\n" +
+            "                    automatic transmission.\n" +
+            "                    <br><br>Our <span class=\"blue\">driving instructors</span> will help you pass the traffic police exam\n" +
+            "                    and\n" +
+            "                    obtain\n" +
+            "                    licenses of categories <span class=\"blue\">A,\n" +
+            "            B, C, D, E</span>. Driving courses are held in the city center and on the left bank within walking distance\n" +
+            "                    from\n" +
+            "                    any transport.\n" +
+            "                    <br><br>You can also choose a convenient time to attend theoretical classes at our <span\n" +
+            "                            class=\"blue\">driving school</span>.",
+        categories: "CATEGORIES",
+        price_k: "PRICE",
+        a1_r:"A1 (RETRAINING)",
+        a_r:"A (RETRAINING)",
+        F1000:"FROM 1000 UAH",
+        F2500:"FROM 2500 UAH",
+        U899:"899 UAH",
+        b_r:"B (RETRAINING)",
+        F1700:"FROM 1700 UAH",
+        F4000:"FROM 4000 UAH",
+        F3900:"FROM 3900 UAH",
+        F4300:"FROM 4300 UAH",
+        our_cars:"OUR CARS",
+        loc:"LOCATION",
+        previous:"Previous",
+        next:"Next",
+        write_comment:"Write a comment",
+        r_comment:"Review",
+        name:"Name",
+        wr_com:"Comment",
+        cancel:"Cancel",
+        submit:"Submit",
+        notification:"Notification",
+        message:"You have already left the maximum number of comments",
+        reg:"Sign up for a course",
+        fullname:"Full name",
+        phone:"Phone Number",
+        desc_cont:"For detailed information about graphics classes, training opportunities and\n" +
+            "                    graphics recruitment at the driving school, you can\n" +
+            "                    get to know the phones:",
+
     },
     ru: {
         about_us: "О нас",
@@ -28,17 +70,54 @@ const translations = {
         contacts: "Контакты",
         comments: "Комментарии",
         language: "Язык",
-        hero_text: "Один клик и\n" +
-            "автомобильные права\n" +
-            "у тебя в кармане",
+        hero_text: "Один клик и<br>автомобильные права<br>у тебя в кармане",
         button_sing_up: "Запишитесь на курс",
-        q_i:"Опытные инструкторы",
-        f_t_s:"Гибкий график обучения",
-        h_f_e:"Помощь с экзаменами",
-        m_e_m:"Актуальные материалы",
-        m_v_f:"Современный автопарк",
-        c_p:"Конкурентные цены",
-        about_us_hero:""
+        q_i: "Опытные инструкторы",
+        f_t_s: "Гибкий график обучения",
+        h_f_e: "Помощь с экзаменами",
+        m_e_m: "Актуальные материалы",
+        m_v_f: "Современный автопарк",
+        c_p: "Конкурентные цены",
+        about_us_main_text: "О <span class='blue'>Нас</span>",
+        about_us_sub_text1: "<br><br>Рады приветствовать Вас на сайте автошколы в Днепре <span class=\"blue\">\"Гранд\"</span>.\n" +
+            " <br><br>С нами вы научитесь <span class=\"blue\">эффективно управлять</span> автомобилем с\n" +
+            " механической или\n" +
+            " автоматической коробкой передач.\n" +
+            " <br><br>Наши <span class=\"blue\">инструкторы по вождению</span> помогут Вам сдать экзамен ГИБДД\n" +
+            " и\n" +
+            " получать\n" +
+            " лицензии категорий <span class=\"blue\">А,\n" +
+            " Б, В, Г, Е</span>. Курсы вождения проводятся в центре города и на левом берегу в шаговой доступности.\n" +
+            " от\n" +
+            " любого транспорта.\n",
+        categories: "КАТЕГОРИИ",
+        price_k: "ЦЕНА",
+        a1_r:"A1 (Переподг.)",
+        a_r:"A (Переподг)",
+        F1000:"от 1000 грн",
+        F2500:"от 2500 грн",
+        U899:"899 грн",
+        b_r:"B (Переподг.)",
+        F1700:"от 1700 грн",
+        F4000:"от 4000 грн",
+        F3900:"от 3900 грн",
+        F4300:"от 4300 грн",
+        our_cars:"Автопарк",
+        loc:"Филиалы",
+        previous:"Предыдущий",
+        next:"Следующий",
+        write_comment:"Написать комментарий",
+        r_comment:"Отзыв",
+        name:"Имя",
+        wr_com:"Комментарий",
+        cancel: "Отменить",
+        submit: "Отправить",
+        notification:"Уведомление",
+        message:"Вы уже оставили максимальное количество комментариев",
+        reg:"Записаться на курс",
+        fullname:"Имя и фамилия",
+        phone:"Номер телефона",
+        desc_cont:"Подробную информацию о занятиях графикой, возможностях обучения и наборе графики в автошколу вы можете узнать по телефонам:",
     },
     ua: {
         about_us: "Про нас",
@@ -49,24 +128,61 @@ const translations = {
         contacts: "Контакти",
         comments: "Коментарі",
         language: "Мова",
-        hero_text: "Один клік і\n" +
-            "водійські  права\n" +
-            "у твоїй  кишені",
+        hero_text: "Один клік і<br>водійські  права<br>у твоїй  кишені",
         button_sing_up: "Запишіться на курс",
-        q_i:"Кваліфіковані інструктори",
-        f_t_s:"Гнучкий графік навчання",
-        h_f_e:"Допомога з іспитіами",
-        m_e_m:"Сучасні матеріали",
-        m_v_f:"Модернізований автопарк",
-        c_p:"Конкурентні ціни",
-        about_us_hero:""
+        q_i: "Кваліфіковані інструктори",
+        f_t_s: "Гнучкий графік навчання",
+        h_f_e: "Допомога з іспитіами",
+        m_e_m: "Сучасні матеріали",
+        m_v_f: "Модернізований автопарк",
+        c_p: "Конкурентні ціни",
+        about_us_main_text: "Про <span class='blue'>Нас</span>",
+        about_us_sub_text1: "<br><br>Ради вітати Вас на сайті автошколи Дніпра <span class=\"blue\">«Гранд»</span>.\n" +
+            " <br><br>З нами ви навчитеся <span class=\"blue\">ефективно керувати</span> автомобілем з\n" +
+            " механічною або\n" +
+            " автоматичною коробкою передач.\n" +
+            " <br><br>Наші <span class=\"blue\">інструктори з водіння</span> допоможуть вам скласти іспит в ДАІ\n" +
+            " і\n" +
+            " отримати\n" +
+            " ліцензії категорії <span class=\"blue\">A,\n" +
+            " B, C, D, E</span>. Курси водіння проходять в центрі міста та на лівому березі в пішій доступності\n" +
+            " від\n" +
+            " будь-якого транспорту.",
+        categories: "КАТЕГОРІЇ",
+        price_k: "ЦІНА",
+        a1_r:"A1 (Перепідг.)",
+        a_r:"A (Перепідг.)",
+        F1000:"від 1000 грн",
+        F2500:"від 2500 грн",
+        U899:"899 грн",
+        b_r:"B (Перепідг.)",
+        F1700:"від 1700 грн",
+        F4000:"від 4000 грн",
+        F3900:"від 3900 грн",
+        F4300:"від 4300 грн",
+        our_cars:"Автопарк",
+        loc:"Філії",
+        previous:"Попередній",
+        next:"Наступний",
+        write_comment:"Написати коментар",
+        r_comment:"Відгук",
+        name:"Ім'я",
+        wr_com:"Коментар",
+        cancel: "Відмінити",
+        submit: "Відправити",
+        notification:"Сповіщення",
+        message:"Ви вже залишили максимальну кількість коментарів",
+        reg:"Записатися на курс",
+        fullname:"Прізвище та ім'я",
+        phone:"Номер телефона",
+        desc_cont:"Детальну інформацію про курси графіки, можливості навчання та набір графіків в автошколі можна отримати за телефонами:",
     }
 }
 
 function changeLanguage(language) {
     document.querySelectorAll('[data-key]').forEach(element => {
         const key = element.getAttribute('data-key');
-        element.innerText = translations[language][key];
+        element.innerHTML = translations[language][key];
     });
 }
 
