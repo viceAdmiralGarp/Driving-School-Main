d875e732b5d133d883a1f30a7bbdef8a667beb13
viceAdmiralGarp <vyacheslavtihonov636@gmail.com>
2024-07-09 21:36:02 +0300

    lol

--- mmdev/js/admin.js
+++
@@ -1,5 +0,0 @@
-function updateIndexPage(){
-    let A1ret = document.getElementById('A1ret').value;
-    localStorage.setItem('A1retValue',A1ret);
-    alert("Цены успешно установлены")
-}
--- mmdev/js/price.js
+++
@@ -1,8 +0,0 @@
-function updatePrice(){
-    let newPrice = localStorage.getItem('A1retValue');
-    if(newPrice==null){
-        document.querySelector('.A1ret').textContent = newPrice;
-    }
-}
-
-window.onload = updatePrice;
